c3490d8d214fa401068346a37a48ba64f58f36d5
librekcesar <140665209+librekcesar@users.noreply.github.com>
2024-09-19 08:33:20 -0600

    Add files via upload

---
+++ bodega_periferico.js
@@ -0,0 +1,8 @@
+var peri = {
+"type": "FeatureCollection",
+"name": "bodega_periferico",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "nombre": "Periferico", "direccion": "Av Industria Textil 4, Zapopan Industrial, 45132 Zapopan, Jal.", "km_dist": 7.655 }, "geometry": { "type": "Point", "coordinates": [ -103.391207291620873, 20.740801651978533 ] } }
+]
+}
